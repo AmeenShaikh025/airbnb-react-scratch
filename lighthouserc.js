953225a63258e24e9c05efedbdf1753901b55928
AmeenShaikh025 <ameenshaikh25031994@gmail.com>
2020-12-15 19:00:59 +0530

    removedlighthouse asert

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,12 +5,12 @@ module.exports = {
             startServerCommand: 'npm run start',
             url: ['http://localhost:8080']
 		},
-		assert: {
-			assertions: {
-			  'categories:performance': ['warn', {minScore: 1}],
-			  'categories:accessibility': ['error', {minScore: 1}]
-			}
-		  },
+		// assert: {
+		// 	assertions: {
+		// 	  'categories:performance': ['warn', {minScore: 1}],
+		// 	  'categories:accessibility': ['error', {minScore: 1}]
+		// 	}
+		//   },
 		upload: {
             /* Add configuration here */
             target: 'temporary-public-storage',
