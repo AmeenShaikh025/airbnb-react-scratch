03b7d2c4434c0a136f3816fece085ad4e9e5bf6c
AmeenShaikh025 <ameenshaikh25031994@gmail.com>
2020-12-24 09:27:10 +0530

    simple test case added along with typescript moifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-import banner from "./assets/banner.jpg";
+// import banner from "./assets/banner.jpg";
 // const banner =  require("./assets/banner.jpg")
 
 function App() {
     return (
         <div>
             <h2>React app </h2>
-            <img src={banner} width="100%" alt="banner image"/>
+            {/* <img src={banner} width="100%" alt="banner image"/> */}
         </div>
     )
 }
 
 export default App
 
+
+export function sum(a, b) {
+    return a + b;
+}
