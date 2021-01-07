60ee0e876ac4a77e1803f6fe92f854d274f16122
AmeenShaikh025 <ameenshaikh25031994@gmail.com>
2021-01-07 21:35:57 +0530

    added scroll event for the navbar

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useRef, useEffect } from 'react'
+import { Link, useHistory } from "react-router-dom";
 
 import "./Header.css"
 
@@ -8,12 +8,94 @@ interface pageName {
 }
 
 const Header = (props: pageName) => {
+    
     const navColorClass = props.page === "explore" ? "nav-colored" : "";
+
+    let history = useHistory();
+
+    //reference for dropdown
+    const header: any = useRef();
+    const nav: any = useRef();
+    const bottomNav: any = useRef();
+    const dropdownCountry: any = useRef();
+    const dropdownUser: any = useRef();
+
+    const handleCountryDropdown = (e: any) => {
+        let grandParent = e.target?.parentNode?.parentNode;
+        let parent = e.target?.parentNode;
+        let result = parent.classList.contains("dropdown__country");
+        if(grandParent.classList.contains("dropdown__country") || e.target.classList.contains("dropdown__country") || parent.classList.contains("dropdown__country") ) {
+            dropdownCountry.current.classList.toggle("show");
+            dropdownUser.current.classList.remove("show")
+        }
+    }
+
+    const handleUserDropdown = (e: any) => {
+        let grandParent = e.target?.parentNode?.parentNode;
+        let parent = e.target?.parentNode;
+        let result = parent.classList.contains("dropdown__user");
+        if(grandParent.classList.contains("dropdown__user") || e.target.classList.contains("dropdown__user") || parent.classList.contains("dropdown__user") ) {
+            dropdownUser.current.classList.toggle("show");
+            dropdownCountry.current.classList.remove("show")
+        }
+    }
+
+    useEffect(() => {
+        let lastScroll = 0, scrolledDistance = 0;
+        
+        window.onscroll = () => {
+            
+            let currentScroll = window.pageYOffset;
+
+            //run the code if the page is home page
+            if(history.location.pathname === "/") {
+               
+                if(window.pageYOffset >= header.current.offsetHeight) {
+                    header.current.classList.add("nav-colored");
+                    header.current.classList.remove("nav-transparent");
+                }
+                else {
+                    header.current.classList.add("nav-transparent");
+                    header.current.classList.remove("nav-colored");
+                }
+                
+                // Menu Bar
+                
+    
+                // Top nav
+                if (currentScroll > 20) {
+                    nav.current.classList.add("hide__nav");
+                    nav.current.classList.remove("show__nav");
+                } else {
+                    nav.current.classList.add("show__nav");
+                    nav.current.classList.remove("hide__nav");
+                }
+
+            }
+            
+            scrolledDistance = lastScroll - currentScroll;
+
+            // Bottom nav
+            if(currentScroll < lastScroll) {
+                lastScroll = currentScroll;
+                //"Scrolling UP"
+                if(scrolledDistance <= 3) {
+                    bottomNav.current.style.transform = "translateY(0)"
+                }
+            } else {
+                lastScroll = currentScroll;
+                //"Scrolling DOWN"
+                if(scrolledDistance >= -6) {
+                    bottomNav.current.style.transform = "translateY(100px)"
+                }
+            }
+        }
+    }, []);
 
     return (
         <>
-            <header id="header" className={navColorClass}>
-                <nav className="nav">
+            <header id="header" className={navColorClass} ref={header}>
+                <nav className="nav" ref={nav}>
                     <div className="nav__left">
                         <Link to="/">
                             <svg width="102" height="32">
@@ -34,31 +116,15 @@ const Header = (props: pageName) => {
                             </Link>
                         </nav>
                     </div>
-                    <div className="bottom__mobile__nav d-sm-none d-block">
-                        <nav className="sub__nav">
-                            <Link to="/">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                    <path className="active-tab" fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-                                </svg>
-                            </Link>
-                            <Link to="/">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="route" className="svg-inline--fa fa-route fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M416 320h-96c-17.6 0-32-14.4-32-32s14.4-32 32-32h96s96-107 96-160-43-96-96-96-96 43-96 96c0 25.5 22.2 63.4 45.3 96H320c-52.9 0-96 43.1-96 96s43.1 96 96 96h96c17.6 0 32 14.4 32 32s-14.4 32-32 32H185.5c-16 24.8-33.8 47.7-47.3 64H416c52.9 0 96-43.1 96-96s-43.1-96-96-96zm0-256c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zM96 256c-53 0-96 43-96 96s96 160 96 160 96-107 96-160-43-96-96-96zm0 128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg>
-                            </Link>
-                            <Link to="/">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-                            </Link>
-                        </nav>
-                    </div>
                     <div className="nav__right">
                         <nav className="dropdown__nav">
                             <div className="dropdown">
-                                <Link to="" className="dropdown__button dropdown__country">
-                                    
+                                <span className="dropdown__button dropdown__country" onClick={handleCountryDropdown}>
                                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="globe-asia" className="svg-inline--fa fa-globe-asia fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm-11.34 240.23c-2.89 4.82-8.1 7.77-13.72 7.77h-.31c-4.24 0-8.31 1.69-11.31 4.69l-5.66 5.66c-3.12 3.12-3.12 8.19 0 11.31l5.66 5.66c3 3 4.69 7.07 4.69 11.31V304c0 8.84-7.16 16-16 16h-6.11c-6.06 0-11.6-3.42-14.31-8.85l-22.62-45.23c-2.44-4.88-8.95-5.94-12.81-2.08l-19.47 19.46c-3 3-7.07 4.69-11.31 4.69H50.81C49.12 277.55 48 266.92 48 256c0-110.28 89.72-200 200-200 21.51 0 42.2 3.51 61.63 9.82l-50.16 38.53c-5.11 3.41-4.63 11.06.86 13.81l10.83 5.41c5.42 2.71 8.84 8.25 8.84 14.31V216c0 4.42-3.58 8-8 8h-3.06c-3.03 0-5.8-1.71-7.15-4.42-1.56-3.12-5.96-3.29-7.76-.3l-17.37 28.95zM408 358.43c0 4.24-1.69 8.31-4.69 11.31l-9.57 9.57c-3 3-7.07 4.69-11.31 4.69h-15.16c-4.24 0-8.31-1.69-11.31-4.69l-13.01-13.01a26.767 26.767 0 0 0-25.42-7.04l-21.27 5.32c-1.27.32-2.57.48-3.88.48h-10.34c-4.24 0-8.31-1.69-11.31-4.69l-11.91-11.91a8.008 8.008 0 0 1-2.34-5.66v-10.2c0-3.27 1.99-6.21 5.03-7.43l39.34-15.74c1.98-.79 3.86-1.82 5.59-3.05l23.71-16.89a7.978 7.978 0 0 1 4.64-1.48h12.09c3.23 0 6.15 1.94 7.39 4.93l5.35 12.85a4 4 0 0 0 3.69 2.46h3.8c1.78 0 3.35-1.18 3.84-2.88l4.2-14.47c.5-1.71 2.06-2.88 3.84-2.88h6.06c2.21 0 4 1.79 4 4v12.93c0 2.12.84 4.16 2.34 5.66l11.91 11.91c3 3 4.69 7.07 4.69 11.31v24.6z">
                                         </path>
                                     </svg>
-                                </Link>
-                                <div className="dropdown__content dropdown__1">
+                                </span>
+                                <div className="dropdown__content dropdown__1" ref={dropdownCountry}>
                                     <Link to="/">
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="globe-asia" className="svg-inline--fa fa-globe-asia fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm-11.34 240.23c-2.89 4.82-8.1 7.77-13.72 7.77h-.31c-4.24 0-8.31 1.69-11.31 4.69l-5.66 5.66c-3.12 3.12-3.12 8.19 0 11.31l5.66 5.66c3 3 4.69 7.07 4.69 11.31V304c0 8.84-7.16 16-16 16h-6.11c-6.06 0-11.6-3.42-14.31-8.85l-22.62-45.23c-2.44-4.88-8.95-5.94-12.81-2.08l-19.47 19.46c-3 3-7.07 4.69-11.31 4.69H50.81C49.12 277.55 48 266.92 48 256c0-110.28 89.72-200 200-200 21.51 0 42.2 3.51 61.63 9.82l-50.16 38.53c-5.11 3.41-4.63 11.06.86 13.81l10.83 5.41c5.42 2.71 8.84 8.25 8.84 14.31V216c0 4.42-3.58 8-8 8h-3.06c-3.03 0-5.8-1.71-7.15-4.42-1.56-3.12-5.96-3.29-7.76-.3l-17.37 28.95zM408 358.43c0 4.24-1.69 8.31-4.69 11.31l-9.57 9.57c-3 3-7.07 4.69-11.31 4.69h-15.16c-4.24 0-8.31-1.69-11.31-4.69l-13.01-13.01a26.767 26.767 0 0 0-25.42-7.04l-21.27 5.32c-1.27.32-2.57.48-3.88.48h-10.34c-4.24 0-8.31-1.69-11.31-4.69l-11.91-11.91a8.008 8.008 0 0 1-2.34-5.66v-10.2c0-3.27 1.99-6.21 5.03-7.43l39.34-15.74c1.98-.79 3.86-1.82 5.59-3.05l23.71-16.89a7.978 7.978 0 0 1 4.64-1.48h12.09c3.23 0 6.15 1.94 7.39 4.93l5.35 12.85a4 4 0 0 0 3.69 2.46h3.8c1.78 0 3.35-1.18 3.84-2.88l4.2-14.47c.5-1.71 2.06-2.88 3.84-2.88h6.06c2.21 0 4 1.79 4 4v12.93c0 2.12.84 4.16 2.34 5.66l11.91 11.91c3 3 4.69 7.07 4.69 11.31v24.6z">
                                             </path>
@@ -75,12 +141,12 @@ const Header = (props: pageName) => {
                             </div>
 
                             <div className="dropdown">
-                                <Link to="" className="dropdown__button dropdown__user">
+                                <span className="dropdown__button dropdown__user" onClick={handleUserDropdown}>
                                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
                                         </path>
                                     </svg>
-                                </Link>
-                                <div className="dropdown__content dropdown__2">
+                                </span>
+                                <div className="dropdown__content dropdown__2" ref={dropdownUser}>
                                     <Link to="/">
                                         <strong>
                                             Sign Up
@@ -91,6 +157,21 @@ const Header = (props: pageName) => {
                                     </Link>
                                 </div>
                             </div>
+                        </nav>
+                    </div>
+                    <div className="bottom__mobile__nav d-sm-none d-block" ref={bottomNav}>
+                        <nav className="sub__nav">
+                            <Link to="/" aria-label="search" className="nav-link" href="javascript:void(0);">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" className="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path className="active-tab" fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+                                </svg>
+                            </Link>
+                            <Link to="/explore" aria-label="map" className="nav-link" href="javascript:void(0);">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="route" className="svg-inline--fa fa-route fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M416 320h-96c-17.6 0-32-14.4-32-32s14.4-32 32-32h96s96-107 96-160-43-96-96-96-96 43-96 96c0 25.5 22.2 63.4 45.3 96H320c-52.9 0-96 43.1-96 96s43.1 96 96 96h96c17.6 0 32 14.4 32 32s-14.4 32-32 32H185.5c-16 24.8-33.8 47.7-47.3 64H416c52.9 0 96-43.1 96-96s-43.1-96-96-96zm0-256c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zM96 256c-53 0-96 43-96 96s96 160 96 160 96-107 96-160-43-96-96-96zm0 128c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg>
+                            </Link>
+                            <Link to="/" aria-label="ratings" className="nav-link" href="javascript:void(0);">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="svg-inline--fa fa-star fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
+                            </Link>
                         </nav>
                     </div>
                 </nav>
